598fb1f6ccc8ac4f2aae3810b8b63b485aa0649c
sumitt60 <sumitt60@gmail.com>
2022-07-31 17:46:24 +0530

    Commit Package2 Copy

---
+++ src/package2Copy/copy/Package2Test.java
@@ -0,0 +1,24 @@
+package package2Copy.copy;
+
+import org.testng.annotations.Test;
+
+public class Package2Test {
+	
+	
+
+	@Test
+	public void TestMethod()
+	{
+		int count = 0;
+		for(int i =  0; i<=10; i++)
+		{
+		System.out.println("Say Hi!");
+		count = count+1;
+		//System.out.println(count);
+		}
+		System.out.println(count);
+		//return count;
+		//Commit
+	}
+	
+}
